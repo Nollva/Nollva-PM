6b0d6bc194bf06e7e150cd942c80f86a4da857f9
Nollva <63668568+Nollva@users.noreply.github.com>
2025-10-06 15:37:05 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,12 +5,6 @@ import os
 # 🚨 CRITICAL FIX: HARDCODE CONFIGURATION HERE 🚨
 # This bypasses the need for the missing 'dotenv' package.
 
-# 1. SECRET KEY (CRITICAL: Insert your actual long, random string here)
-os.environ['SECRET_KEY'] = 'd477e6d6acc65cd6822da6d30af0183b73f24e900c371f33a1cf97a8a1a38a81'
-
-# 2. DATABASE PATH (Insert your file's ABSOLUTE path)
-os.environ['DB_URI'] = 'mongodb+srv://djkolls:E8L2uvOOjBToSHGq@pmdatabase.rvzzb5u.mongodb.net/?retryWrites=true&w=majority&appName=PMDatabase' 
-
 # 3. FLASK_ENV (Required)
 os.environ['FLASK_ENV'] = 'production'
 
